2c7b48a690b94944870910fd0035bb33521c2b97
Philip Walton <philip@philipwalton.com>
2013-01-13 22:01:33 -0800

    Allow code blocks to specify a language. [closes #28]

--- _plugins/highlightjs.rb
+++ _plugins/highlightjs.rb
@@ -3,14 +3,22 @@ require 'cgi'
 module Jekyll
   class HighlightJsBlock < Liquid::Block
 
+    SYNTAX = /^([a-z]*)$/
+
     def initialize(tag_name, args, tokens)
       super
+      if args.strip =~ SYNTAX
+        @lang = $1
+      else
+        raise SyntaxError.new("Syntax Error in 'highlightjs' - Valid syntax: highlightjs [lang]")
+      end
     end
 
     def render(context)
       code = CGI.escapeHTML(super)
       code = code.gsub(/\*\*(.+)\*\*/, "<em>\\1</em>")
-      "<pre class=\"highlight\"><code>#{code.strip}</code></pre>"
+      cls = @lang ? " class=\"language-#{@lang}\"" : ""
+      "<pre class=\"highlight\"><code#{cls}>#{code.strip}</code></pre>"
     end
 
   end

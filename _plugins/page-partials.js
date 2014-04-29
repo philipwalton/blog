0d4da9fdde77d4e832058c90805b5e4f7d5754fe
Philip Walton <philip@philipwalton.com>
2014-04-28 22:41:47 -0700

    Add initial page partial plugin.

---
+++ _plugins/page-partials.js
@@ -0,0 +1,25 @@
+var _ = require('lodash-node/modern')
+
+module.exports = function() {
+
+  var Page = this.Page;
+  var pages = Page.all();
+
+  this.events.on('beforeRender', function() {
+
+    pages.forEach(function(p) {
+
+      var template = p.template.clone();
+      var post = p.post
+
+      // Remove the default layout from the template layout chain
+      // so create a partial
+
+      var page = new Page(post || template, p.config);
+      page.permalink = page.permalink.clone().append('/_index.html');
+
+    });
+
+  });
+
+};

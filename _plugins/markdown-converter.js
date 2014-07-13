0f0496b21504cd437608300a63ebfb638112af18
Philip Walton <philip@philipwalton.com>
2014-07-13 16:09:47 -0700

    Add smart quotes support.

--- _plugins/markdown-converter.js
+++ _plugins/markdown-converter.js
@@ -1,35 +1,35 @@
-var marked = require('marked')
-var hljs = require("highlight.js")
-var he = require('he')
+var marked = require('marked');
+var hljs = require("highlight.js");
+var he = require('he');
 
 module.exports = function() {
 
-  var events = this.events
-  var r = new marked.Renderer()
+  var events = this.events;
+  var renderer = new marked.Renderer();
 
-  r.code = function(code, lang) {
+  renderer.code = function(code, lang) {
 
     var cls = lang && lang != 'text' ? ' class="' + lang + '"' : ''
 
     if (lang == null) {
-      code = hljs.highlightAuto(code).value
+      code = hljs.highlightAuto(code).value;
     }
     // if lang is "text" then don't highlight
     else if (lang != 'text') {
-      code = hljs.highlight(lang, code).value
+      code = hljs.highlight(lang, code).value;
     }
     else {
       // since we're not using highlight.js here, we need to espace the html
       // unescape first in order to avoid double escaping
-      code = he.escape(he.unescape(code))
+      code = he.escape(he.unescape(code));
     }
 
     // Allow for highlighting portions of code blocks
     // using `**` before and after
-    code = code.replace(/\*\*(.+)?\*\*/g, '<em>$1</em>')
+    code = code.replace(/\*\*(.+)?\*\*/g, '<em>$1</em>');
 
-    return '<pre class="highlight"><code' + cls + '>' + code + '</code></pre>'
-  }
+    return '<pre class="highlight"><code' + cls + '>' + code + '</code></pre>';
+  };
 
   events.on('afterRenderContent', function(p) {
     // if (p.data.format == 'markdown') {
@@ -37,8 +37,11 @@ module.exports = function() {
       // TODO: changing the extention should be automatically done
       // by the Permalink object.
       p.permalink = p.permalink.replace(/\.md$/, '.html')
-      p.content = marked(p.content, {renderer: r})
+      p.content = marked(p.content, {
+        renderer: renderer,
+        smartypants: true
+      });
     }
-  })
+  });
 
-}
+};

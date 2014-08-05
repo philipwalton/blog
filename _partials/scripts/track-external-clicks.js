f8ed1ab548cb24e9cdb8a0cf220cf6ceca4f9c25
Philip Walton <philip@philipwalton.com>
2014-08-05 10:37:55 -0700

    Remove the non-interaction param from event and social hits.

--- _partials/scripts/track-external-clicks.js
+++ _partials/scripts/track-external-clicks.js
@@ -14,11 +14,7 @@
         // Opening links in an external tabs allows the ga beacon to send.
         // When following links directly, sometimes they don't make it.
         this.target = '_blank';
-        ga('send', 'event', 'Outbound Link', 'click', this.href, {
-          // `nonInteracdtion` prevents this event from affecting bounce rate.
-          // https://developers.google.com/analytics/devguides/collection/analyticsjs/events
-          'nonInteraction': 1
-        });
+        ga('send', 'event', 'Outbound Link', 'click', this.href);
       }
     });
 
--- _partials/scripts/track-social-interactions.js
+++ _partials/scripts/track-social-interactions.js
@@ -8,11 +8,7 @@
       var socialAction = this.getAttribute('data-social-action');
       var socialTarget = location.href;
 
-      ga('send', 'social', socialNetwork, socialAction, socialTarget, {
-        // `nonInteracdtion` prevents this event from affecting bounce rate.
-        // https://developers.google.com/analytics/devguides/collection/analyticsjs/events
-        'nonInteraction': 1
-      });
+      ga('send', 'social', socialNetwork, socialAction, socialTarget);
     }
   });
 

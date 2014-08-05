94193fe2f987d463128dae39be7cf374b7b34da0
Philip Walton <philip@philipwalton.com>
2014-08-04 22:48:28 -0700

    Simplify social tracking.

--- _partials/scripts/track-social-interactions.js
+++ _partials/scripts/track-social-interactions.js
@@ -6,12 +6,8 @@
     var socialNetwork = this.getAttribute('data-social-network');
     if (socialNetwork) {
       var socialAction = this.getAttribute('data-social-action');
-      var socialTarget = this.getAttribute('data-social-target');
-
-      // If the url is relative to the current host, expand it.
-      if (socialTarget.charAt(0) == '/') {
-        socialTarget = 'http://philipwalton.com' + socialTarget;
-      }
+      var socialTarget = 'http://philipwalton.com'
+          + this.getAttribute('data-social-target');
 
       ga('send', 'social', socialNetwork, socialAction, socialTarget, {
         // `nonInteracdtion` prevents this event from affecting bounce rate.

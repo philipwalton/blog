73e32a164d2458a4435667af196452e4eb775811
Philip Walton <philip@philipwalton.com>
2014-08-05 00:02:42 -0700

    Update order of scripts.

--- _partials/scripts/google-analytics-setup.js
+++ _partials/scripts/google-analytics-setup.js
@@ -1,12 +1,10 @@
-{{#is site.env 'production'}}
-  {{#if site.googleAnalyticsTrackingID}}
-    ga = function() {
-      ga.q.push(arguments);
-    };
-    ga.q = [
-      ['create', '{{site.googleAnalyticsTrackingID}}', 'auto'],
-      ['send', 'pageview']
-    ];
-    ga.l = 1 * new Date();
-  {{/if}}
-{{/is}}
+{{#if site.googleAnalyticsTrackingID}}
+  ga = function() {
+    ga.q.push(arguments);
+  };
+  ga.q = [
+    ['create', '{{site.googleAnalyticsTrackingID}}', 'auto'],
+    ['send', 'pageview']
+  ];
+  ga.l = 1 * new Date();
+{{/if}}

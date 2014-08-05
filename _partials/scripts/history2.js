d61cfa5bee701d4935609cb47acb88ca4206d0fc
Philip Walton <philip@philipwalton.com>
2014-08-05 14:57:39 -0700

    Separate parseUrl into its own module.

--- _partials/scripts/history2.js
+++ _partials/scripts/history2.js
@@ -2,31 +2,6 @@ var history2 = (function(window, document, location) {
 
   if (!(window.history && window.history.pushState)) return;
 
-  /**
-   * Parse the given url and return the properties returned
-   * by the `window.location` object (hash, href, pathname, and search).
-   * @param {string} url - The url to parse.
-   * @return {Object} An object with the same keys as `window.location`.
-   */
-  // TODO, abstract this into a generic URL parsing utility. Keep this in mind:
-  // http://blogs.msdn.com/b/ieinternals/archive/2011/02/28/internet-explorer-window-location-pathname-missing-slash-and-host-has-port.aspx
-  var parseUrl = (function(a) {
-    var cache = {};
-    return function parseUrl(url) {
-      return cache[url]
-        ? cache[url]
-        : cache[a.href = url] = {
-            hash: a.hash,
-            href: a.href,
-            // Sometimes IE doesn't include the leading slash for pathname.
-            // http://stackoverflow.com/questions/956233/javascript-pathname-ie-quirk
-            pathname: a.pathname.charAt(0) == '/'
-              ? a.pathname : '/' + a.pathname,
-            search: a.search
-          };
-    };
-  }(document.createElement('a')));
-
 
   // Store the current url information.
   var current = parseUrl(location.href);

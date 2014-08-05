e7fb7915ffe5b46ebcc5fbcc6ec21348e8e8f1e8
Philip Walton <philip@philipwalton.com>
2014-08-04 22:03:12 -0700

    Don't depend on the body variable being in the outer scope.

--- _partials/scripts/legacy-test.js
+++ _partials/scripts/legacy-test.js
@@ -1,9 +1,10 @@
 // Add an "is-legacy" class to the body for
 // browsers that don't support flexbox.
-(function(style) {
+(function(body) {
+  var style = body.style;
   if (!('flexBasis' in style ||
       'msFlexAlign' in style ||
       'webkitBoxDirection' in style)) {
     body.className += ' is-legacy';
   }
-}(body.style));
+}(document.body));

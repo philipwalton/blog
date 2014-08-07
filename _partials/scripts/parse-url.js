b935b69250d6e185b7407c4953841c2677dfaf80
Philip Walton <philip@philipwalton.com>
2014-08-06 22:28:22 -0700

    Account for weird IE bugs in the parseUrl module.

--- _partials/scripts/parse-url.js
+++ _partials/scripts/parse-url.js
@@ -1,26 +1,39 @@
 /**
  * Parse the given url and return the properties returned
- * by the `window.location` object.
+ * by the `Location` object with the addition of the `path` property.
+ * `path` is the concatenation of the `pathname` and `search` properties.
  * @param {string} url - The url to parse.
  * @return {Object} An object with the same keys as `window.location`.
  */
 var parseUrl = (function(a) {
   var cache = {};
   return function parseUrl(url) {
-    return cache[url] ? cache[url] : cache[a.href = url] = {
+
+    if (cache[url]) return cache[url];
+
+    a.href = url;
+
+    // Sometimes IE incorrectly includes the port ':80' even when no port
+    // is specified in the URL.
+    // http://blogs.msdn.com/b/ieinternals/archive/2011/02/28/internet-explorer-window-location-pathname-missing-slash-and-host-has-port.aspx
+    var host = url.indexOf(a.host) < 0 ? a.host.replace(':80', '') : a.host;
+
+    // Not all browser support `origin` so we have to build it.
+    var origin = a.origin ? a.origin : a.protocol + '//' + a.host;
+
+    // Sometimes IE doesn't include the leading slash for pathname.
+    // http://blogs.msdn.com/b/ieinternals/archive/2011/02/28/internet-explorer-window-location-pathname-missing-slash-and-host-has-port.aspx
+    var pathname = a.pathname.charAt(0) == '/' ? a.pathname : '/' + a.pathname;
+
+    return cache[url] = {
       hash: a.hash,
-      // Sometimes IE incorrectly includes the port ':80' even when no port
-      // is specified in the URL.
-      // http://blogs.msdn.com/b/ieinternals/archive/2011/02/28/internet-explorer-window-location-pathname-missing-slash-and-host-has-port.aspx
-      host: url.indexOf(a.host) < 0 ? a.host.replace(':80', '') : a.host,
+      host: host,
       hostname: a.hostname,
       href: a.href,
-      // Not all browser support `origin` so we have to build it.
-      origin: a.origin ? a.origin : a.protocol + '//' + a.host,
+      origin: origin,
+      path: pathname + a.search,
+      pathname: pathname,
       protocol: a.protocol,
-      // Sometimes IE doesn't include the leading slash for pathname.
-      // http://blogs.msdn.com/b/ieinternals/archive/2011/02/28/internet-explorer-window-location-pathname-missing-slash-and-host-has-port.aspx
-      pathname: a.pathname.charAt(0) == '/' ? a.pathname : '/' + a.pathname,
       search: a.search
     };
   };

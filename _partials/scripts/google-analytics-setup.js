49e89d80ffa9345d9de5ab1e7ffb2028457f9bcd
Philip Walton <philip@philipwalton.com>
2014-08-06 22:26:23 -0700

    Override the sendHitTask in development to log measurement protocol requests to the console.

--- _partials/scripts/google-analytics-setup.js
+++ _partials/scripts/google-analytics-setup.js
@@ -1,10 +1,25 @@
 {{#if site.googleAnalyticsTrackingID}}
-  ga = function() {
-    ga.q.push(arguments);
-  };
-  ga.q = [
-    ['create', '{{site.googleAnalyticsTrackingID}}', 'auto'],
-    ['send', 'pageview']
-  ];
-  ga.l = 1 * new Date();
+
+  ga = function() { ga.q.push(arguments); };
+  ga.q = []; ga.l = 1 * new Date();
+
+  ga('create', '{{site.googleAnalyticsTrackingID}}', 'auto');
+
+  {{#is site.env 'development'}}
+    // In non-production mode, simply log GA hits to the console.
+    ga(function(tracker) {
+      tracker.set('sendHitTask', function(model) {
+        var payload = model.get('hitPayload');
+        console.log('%c' + model.get('hitType'), 'font-weight:bold;');
+        console.log(decodeURIComponent(payload).replace(/&/g, '\n&'));
+
+        // Throw to stop subsequent tasks.
+        throw 'Abort tracking in non-production environments.'
+      });
+    });
+  {{/is}}
+
+  // Note, tasks must be set before sending the first hit
+  ga('send', 'pageview');
+
 {{/if}}

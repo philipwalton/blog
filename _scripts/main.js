ec91c4fab43f905719c3f96d8eb6e21efaab570c
Philip Walton <philip@philipwalton.com>
2014-08-27 23:40:46 -0700

    Switch back to loading typekit synchronously in the head.

--- _scripts/main.js
+++ _scripts/main.js
@@ -1,5 +1,4 @@
 var supports = require('./supports');
-var typekit = require('./typekit');
 var outboundLinks = require('./outbound-links');
 var socialInteractions = require('./social-interactions');
 var contentLoader = require('./content-loader');
@@ -9,10 +8,6 @@ var contentLoader = require('./content-loader');
 if (!supports.flexbox()) {
   document.documentElement.className += ' is-legacy';
 }
-
-
-// Load the typekit fonts and trasition in, timeout if taking too long.
-typekit.init();
 
 
 // Track when the user clicks a link to an external site.

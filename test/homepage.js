1bdb53ac33432a3e87e7f1891b937389996fd3bb
Philip Walton <philip@philipwalton.com>
2015-11-27 10:56:27 -0800

    Move the test data into its own file

--- test/homepage.js
+++ test/homepage.js
@@ -1,51 +1,22 @@
 import assert from 'assert';
-
-
-const TITLE_SUFFIX = ' \u2014 Philip Walton';
-
-
-const ARTICLES = [
-  'Do We Actually Need Specificity In CSS?',
-  'How to Become a Great Front-End Engineer',
-  'Extending Styles',
-  'Side Effects in CSS',
-  'Normalizing Cross-browser Flexbox Bugs',
-  'Measuring Your Site\'s Responsive Breakpoint Usage',
-  'The Dangers of Stopping Event Propagation',
-  'Stop Copying Social Code Snippets',
-  'Implementing Private and Protected Members in JavaScript',
-  'How to Find Qualified Developers',
-  'Interviewing as a Front-End Engineer in San Francisco',
-  'Solved by Flexbox',
-  'Decoupling Your HTML, CSS, and JavaScript',
-  'Why I Test Private Functions In JavaScript',
-  'How to Unit Test Private Functions in JavaScript',
-  'Introducing HTML Inspector',
-  'CSS: Everything is Global and How to Deal With It',
-  'Dynamic Selectors',
-  'Defending Presentational Class Names',
-  'The Future of OOCSS: A Proposal',
-  'What No One Told You About Z-Index',
-  'CSS Architecture',
-];
+import data from './data';
 
 describe('The home page', function() {
 
   it('should have the right title', function *() {
     let title = yield browser.url('/').getTitle();
-    assert(title, 'Home' + TITLE_SUFFIX);
+    assert.equal(title, 'Home' + data.titleSuffix);
   });
 
-  it('should contain links to all published articles', function *() {
-    let titleEls = yield browser.url('/')
-        .elements('.ArticlePreview-title');
+  it('should contain working links to all published articles', function *() {
+    for (let i = 1, expectedTitle; expectedTitle = data.articles[i-1]; i++) {
+      let selector = `.ArticleList-item:nth-child(${i}) .ArticlePreview-title`;
+      let actualTitle = yield browser.url('/').getText(selector);
+      assert.equal(actualTitle, expectedTitle);
 
-    for (let i = 0, el; el = titleEls.value[i]; i++) {
-      let actualTitle = (yield browser.elementIdText(el.ELEMENT)).value;
-      let expectedTitle = ARTICLES[i];
-      assert(actualTitle, expectedTitle);
+      let pageTitle = yield browser.click(selector).getTitle();
+      assert.equal(pageTitle, expectedTitle + data.titleSuffix);
     }
   });
-
 
 });

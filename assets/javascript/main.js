cffc2800d92e142ebd1294eec78a0966cb72dda6
Philip Walton <philip@philipwalton.com>
2016-02-15 16:27:16 -0800

    Remove legacy browser fallbacks

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,14 +1,7 @@
 require('./analytics');
 
 var contentLoader = require('./content-loader');
-var supports = require('./supports');
 var drawer = require('./drawer');
-
-
-// Adds an `is-legacy` class on browsers that don't supports flexbox.
-if (!supports.flexbox()) {
-  document.documentElement.className += ' is-legacy';
-}
 
 // Loads link via AJAX instead of full page loads in browsers with pushState.
 contentLoader.init();

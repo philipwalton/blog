dfaad7930834a3339f546f2183d705a1fe1b4ad9
Philip Walton <philip@philipwalton.com>
2016-03-08 16:32:38 -0800

    Remove experiment tracking calls

--- assets/javascript/content-loader.js
+++ assets/javascript/content-loader.js
@@ -73,8 +73,6 @@ function setScroll() {
 
 function trackError(error) {
   ga('send', 'exception', {exDescription: error.stack || error.message});
-  ga('control.send', 'exception', {exDescription: error.stack || error.message});
-  ga('testing.send', 'exception', {exDescription: error.stack || error.message});
 }
 
 

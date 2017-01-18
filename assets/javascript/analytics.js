864568c5b04623b21eb9c8bd1bf4156003717109
Philip Walton <philip@philipwalton.com>
2017-01-18 09:34:46 -0800

    Bump tracking version

--- assets/javascript/analytics.js
+++ assets/javascript/analytics.js
@@ -14,7 +14,7 @@ import {breakpoints} from './breakpoints';
  * implementation. This allows you to create a segment or view filter
  * that isolates only data captured with the most recent tracking changes.
  */
-const TRACKING_VERSION = '5';
+const TRACKING_VERSION = '6';
 
 
 /**

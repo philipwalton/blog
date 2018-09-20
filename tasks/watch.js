76ad7a7038e4dcb5288e3bb2431ad22a95cd7988
Philip Walton <philip@philipwalton.com>
2018-09-20 00:39:00 -0700

    Update the watch task

--- tasks/watch.js
+++ tasks/watch.js
@@ -3,7 +3,7 @@ const server = require('./utils/server');
 
 gulp.task('watch', gulp.series('build', 'server', () => {
   gulp.watch('assets/css/**/*.css', gulp.series('css', 'content'));
-  gulp.watch('assets/images/**/*', gulp.series('images'));
+  gulp.watch('assets/images/**/*', gulp.series('images', 'content'));
   gulp.watch('assets/javascript/*.js', gulp.series('javascript', 'content'));
-  gulp.watch('templates/**/*.html', gulp.series('content'));
+  gulp.watch(['templates/**/*.html', 'articles/*.md'], gulp.series('content'));
 }));

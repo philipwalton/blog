5a7987c9dfbb1fb09b70996411f641202b89dae5
Philip Walton <philip@philipwalton.com>
2021-10-01 19:44:03 -0700

    Bump log version

--- assets/javascript/log.js
+++ assets/javascript/log.js
@@ -10,7 +10,7 @@ import {uuid} from './utils/uuid';
  * implementation. This allows you to create a segment or view filter
  * that isolates only data captured with the most recent tracking changes.
  */
-const TRACKING_VERSION = 72;
+const TRACKING_VERSION = 73;
 
 /**
  * A 13-digit, random identifier for the current page.

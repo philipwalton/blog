5160b4d5e3bd73f5cb7a177aa8c93c1d8123d64c
Philip Walton <philip@philipwalton.com>
2024-05-18 20:57:50 -0700

    Update package.json

--- tasks/utils/worker.js
+++ tasks/utils/worker.js
@@ -9,6 +9,7 @@ export const start = async ({verbose = true} = {}) => {
   await new Promise((resolve, reject) => {
     const args = [
       `dev`,
+      `--local`,
       // `--unauthenticated`,
       // `--host=https://localhost.philipwalton.dev`,
     ];

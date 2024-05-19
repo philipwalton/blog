ebaac1880bb775f67337ebe0cc316894819b4205
Philip Walton <philip@philipwalton.com>
2024-05-18 20:46:41 -0700

    Remove missed gulpfile

--- gulpfile.js
+++
@@ -1,7 +0,0 @@
-import './tasks/build.js';
-import './tasks/clean.js';
-import './tasks/content.js';
-import './tasks/server.js';
-import './tasks/sw.js';
-import './tasks/watch.js';
-import './tasks/worker.js';

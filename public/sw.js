60005fac01e396e29e3928540c7d03d8c7ee04e0
Philip Walton <philip@philipwalton.com>
2026-01-17 11:35:07 -0800

    Update service worker cleanup code

--- public/sw.js
+++ public/sw.js
@@ -1,8 +1,12 @@
-// Cleanup service worker to replace any previously installed version.
-// Unregisters itself and clears all caches.
-self.addEventListener('install', () => self.skipWaiting());
+/**
+ * Cleans up service worker to replace any previously installed version.
+ * Unregisters itself and clears all local data.
+ */
+async function cleanup() {
+  // Delete all workbox IDB databases (best-effort, don't await success).
+  const dbs = ['workbox-expiration', 'workbox-background-sync'];
+  dbs.forEach((name) => indexedDB.deleteDatabase(name));
 
-self.addEventListener('activate', async () => {
   // Clear all caches
   const cacheNames = await caches.keys();
   await Promise.all(cacheNames.map((name) => caches.delete(name)));
@@ -13,4 +17,7 @@ self.addEventListener('activate', async () => {
   // Take control and reload clients so they get fresh network responses
   const clients = await self.clients.matchAll({type: 'window'});
   clients.forEach((client) => client.navigate(client.url));
-});
+}
+
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', (e) => e.waitUntil(cleanup()));

c9014f896ce6e20602c22e69cf7a798f7e174f09
Philip Walton <philip@philipwalton.com>
2026-01-29 12:12:47 -0800

    Update syntax highlighting markdown

--- astro.config.ts
+++ astro.config.ts
@@ -1,12 +1,25 @@
 import {defineConfig} from 'astro/config';
 import mdx from '@astrojs/mdx';
+import {transformerMetaWordHighlight} from '@shikijs/transformers';
+import {transformerMetaRangeHighlight} from './src/plugins/transformerMetaRangeHighlight.ts';
 
 export default defineConfig({
   site: 'https://philipwalton.com',
   integrations: [mdx()],
-  // Output static HTML by default
   output: 'static',
   image: {
     layout: 'full-width',
+  },
+  markdown: {
+    shikiConfig: {
+      themes: {
+        light: 'github-light',
+        dark: 'github-dark',
+      },
+      transformers: [
+        transformerMetaRangeHighlight(),
+        transformerMetaWordHighlight(),
+      ],
+    },
   },
 });
